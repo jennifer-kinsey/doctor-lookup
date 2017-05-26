60f9e2790aded656a2aa6eeb6a792a5372ea1169
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-05-26 13:36:52 -0700

    fixed error in getDocs function (neglected to call displayDocs) and used string interpolation. 401 error gone and need to parse through results to get desired data.

--- build/js/app.js
+++ build/js/app.js
@@ -18,12 +18,11 @@ Doctor.prototype.getAllSymptoms = function(displaySymptom){
     });
 };
 
-Doctor.prototype.getDocs = function(symptom){
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ symptom + '& location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=rating-desc&skip=0&limit=20&user_key=' + apiKey)
+Doctor.prototype.getDocs = function(symptom, displayDocs){
+  $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=${symptom}& location=37.773%2C-122.413%2C100&user_location=37.773%2C-122.413&sort=rating-desc&skip=0&limit=20&user_key=${apiKey}`)
     .then(function(response) {
-        displayDocs(response.data);
-        console.log(response.data);
-      })
+      displayDocs(response.data);
+    })
     .fail(function(error) {
       $('#symptoms-form').append("<p>An Error has occurred.</p>");
     });
@@ -60,7 +59,7 @@ $(document).ready(function() {
     if (!yourSymptom){
       $('#result').text("You didn't select anything. Have another go.");
     }
-    doctor.getDocs(displayDocs);
+    doctor.getDocs(yourSymptom, displayDocs);
   });
 
   $('#reset-button').click(function() {
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -5,15 +5,26 @@ var displaySymptom = function(symptom){
     `<label><input type="radio" name="symptom" value="${symptom}">${symptom}</label><br>`);
 };
 
-// var displayResultNum = function(num){
-//   $('#result').append(`<h4>${num} results </h4>`);
-// };
+var makeRow() = function(){
+  $('$result').append(`<div class="row"><div class="doc-info"></div></div>`)
+}
 
 var displayDocs = function(slug){
-  $('#result').append(`<h4>${slug} slug </h4>`);
+  $('.doc-info').last().append(`<h4>${slug} slug </h4>`);
 };
 
 
+// <div class="row"> DONE
+//   <div class="doc-info"> DONE
+//     <h2 class="practice"></h2>
+//     <h3 class="doctor"></h3>
+//     <h3 class="website"></h3>
+//     <h4 class="description"></h4>
+//     <h4 class="accepting"></h4>
+//     <h4 class="address"></h4>
+//     <h4 class="phone"></h4>
+//   </div>
+// </div>
 
 $(document).ready(function() {
   var doctor = new Doctor();
@@ -26,7 +37,7 @@ $(document).ready(function() {
     if (!yourSymptom){
       $('#result').text("You didn't select anything. Have another go.");
     }
-    doctor.getDocs(displayDocs);
+    doctor.getDocs(yourSymptom, displayDocs);
   });
 
   $('#reset-button').click(function() {
